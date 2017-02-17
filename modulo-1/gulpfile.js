fd421077fbe6f47c449db1d0e5a7c7e83a2b4422
Raymi Saldomando <raymi15@gmail.com>
2017-02-17 09:38:33 -0500

    Ejercicio clase 02

--- modulo-1/gulpfile.js
+++ modulo-1/gulpfile.js
@@ -6,19 +6,19 @@ var gulp    = require('gulp'),
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "Clase-01/Solucion",
+        server: "Clase-02/Solucion",
         port: 1500
     });
 
-    gulp.watch("Clase-01/Solucion/*.scss", ['sass']).on('change', browserSync.reload);
-    gulp.watch("Clase-01/Solucion/*.html").on('change', browserSync.reload);
-    gulp.watch("Clase-01/Solucion/*.js").on('change', browserSync.reload);
+    gulp.watch("Clase-02/Solucion/*.scss", ['sass']).on('change', browserSync.reload);
+    gulp.watch("Clase-02/Solucion/*.html").on('change', browserSync.reload);
+    gulp.watch("Clase-02/Solucion/*.js").on('change', browserSync.reload);
 });
 
 gulp.task("sass", function(){
-  return gulp.src('Clase-01/Solucion/*.scss')
+  return gulp.src('Clase-02/Solucion/*.scss')
   .pipe(sass({ outputStyle: 'compressed'}))
-  .pipe(gulp.dest('Clase-01/Solucion/'));
+  .pipe(gulp.dest('Clase-02/Solucion/'));
 });
 
 gulp.task('default', ['serve']);
