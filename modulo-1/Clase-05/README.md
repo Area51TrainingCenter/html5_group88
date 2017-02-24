599f22c900cc51234c0c630c90d928dfcb6c870c
Raymi Saldomando <raymi15@gmail.com>
2017-02-24 01:35:31 -0500

    ejercicios clase05

---
+++ modulo-1/Clase-05/README.md
@@ -0,0 +1,26 @@
+# Instrucciones del Ejercicio
+
+El objetivo de este ejercicio es aplicar lo aprendido en clase. El ejercicio no tiene nota ni tampoco una fecha de entrega.
+
+Está dentro de la responsabilidad del alumno terminarlo ya que le servirá de práctica para reforzar sus conocimientos y contiene los temas que se tomarían en el examen de fin del módulo.
+
+## En que consiste el ejercicio
+
+En el directorio `docs` se encontrarán imagenes de sitios que deben ser replicados en código HTML y usar las propiedades de CSS que se aprendienron en clase. Además, el alumno debe de investigar y utilizar las propiedades que se encuentran en el `README`
+
+Nota: En este ejercicio es fundamental que el alumno utilice lo que aprendió sobre agrupación de elementos a través de las etiquetas HTML5 y `div`
+
+## Lista propiedades para usar en la construcción del sitio
+
+  - `hsl`
+  - `hsla`
+  - `letter-spacing`
+  - `word-spacing`
+  - `text-decoration`
+  - `text-shadow`
+  - `border-image`
+  - `display`
+  - `visibility`
+  - `opacity`
+  - `font-weight`
+  - `font-style`
--- modulo-1/gulpfile.js
+++ modulo-1/gulpfile.js
@@ -3,22 +3,23 @@ var gulp    = require('gulp'),
     sass    = require ('gulp-sass');
     //babel   = require('gulp-babel');
 
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', function() {
 
     browserSync.init({
-        server: "Clase-03/Solucion",
+        server: "Clase-05/Solucion",
         port: 1500
     });
 
-    gulp.watch("Clase-03/Solucion/*.scss", ['sass']).on('change', browserSync.reload);
-    gulp.watch("Clase-03/Solucion/*.html").on('change', browserSync.reload);
-    gulp.watch("Clase-03/Solucion/*.js").on('change', browserSync.reload);
+    //gulp.watch("Clase-05/Solucion/*.scss", ['sass']).on('change', browserSync.reload);
+    gulp.watch("Clase-05/Solucion/css/*.css").on('change', browserSync.reload);
+    gulp.watch("Clase-05/Solucion/*.html").on('change', browserSync.reload);
+    gulp.watch("Clase-05/Solucion/*.js").on('change', browserSync.reload);
 });
 
-gulp.task("sass", function(){
+/*gulp.task("sass", function(){
   return gulp.src('Clase-03/Solucion/*.scss')
   .pipe(sass({ outputStyle: 'compressed'}))
   .pipe(gulp.dest('Clase-03/Solucion/'));
-});
+});*/
 
 gulp.task('default', ['serve']);
